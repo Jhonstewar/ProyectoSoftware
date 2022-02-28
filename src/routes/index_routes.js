aef4c8a6bcdc022cd16bc0e9d2363f81b16e9707
Jhon <jhonstewarnunezcastellanos@gmail.com>
2022-02-28 17:53:11 -0500

    añadir y actualizar

--- src/routes/index_routes.js
+++ src/routes/index_routes.js
@@ -12,9 +12,9 @@ router.get('/',cors(),controller.uno)
 
 router.get('/getInventario',cors(),controller.dos)
 
-router.post('/modInventario',body.urlencoded({extended:false}),controller.insertar)
+router.post('/modInventario',body.urlencoded({extended:false}),controller.modificar)
 
-router.post('/addInventario',body.urlencoded({extended:false}),controller.modificar)
+router.post('/addInventario',body.urlencoded({extended:false}),controller.insertar)
 
 
 module.exports = router
