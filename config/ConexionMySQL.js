468e71fc81e633b34f997b23c0459b1b8a4e86f2
Jhon <jhonstewarnunezcastellanos@gmail.com>
2022-03-11 16:31:14 -0500

    host db para kubernete

--- config/ConexionMySQL.js
+++ config/ConexionMySQL.js
@@ -1,13 +1,14 @@
 
 var mysql = require('mysql');
-
+let hoste = process.env.HOST;
 var connection = mysql.createConnection({
-   host: '172.18.0.2',
+   host: hoste,
    user: 'root',
    password: 'root',
    database: 'ProySof',
    port: 3306
 });
+
 connection.connect(function(error){
    if(error){
       throw error;
